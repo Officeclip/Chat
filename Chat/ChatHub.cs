94d8677f3899cb8f117768e05c2b69d01de06c78
SK Dutta <skd@officeclip.com>
2018-09-18 15:09:24 -0400

    First time partial commit

---
+++ Chat/ChatHub.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace OfficeClip.LiveChat.Chat
+{
+    public class ChatHub : Hub
+    {
+        protected static ChatUser agent = new ChatUser();
+        protected static ChatUser customer = new ChatUser();
+        protected static bool isConnected = false;
+
+        private dynamic AgentConnection
+        {
+            get
+            {
+                return Clients.Client(agent.ConnectionId);
+            }
+        }
+
+        private dynamic CustomerConnection
+        {
+            get
+            {
+                return Clients.Client(customer.ConnectionId);
+            }
+        }
+
+        public void PopulateAgent(string name, string email)
+        {
+            agent.Name = name;
+            agent.Email = email;
+            agent.ConnectionId = Context.ConnectionId;
+            AgentConnection.showWaitingForChat();
+        }
+
+        public void PopulateCustomer(string name, string email)
+        {
+            customer.Name = name;
+            customer.Email = email;
+            customer.ConnectionId = Context.ConnectionId;
+            if (agent.IsAvailable)
+            {
+                AgentConnection.showAcceptForChat();
+                CustomerConnection.showWaitingForAgent();
+            }
+            else
+            {
+                CustomerConnection.showNoAgentAvailable();
+            }
+        }
+
+        public void AgentAccepts()
+        {
+            CustomerConnection.enableChat();
+            CustomerConnection.showAgentAvailableMessage(agent.Name);
+            isConnected = true;
+        }
+
+        private void CustomerRemove()
+        {
+            CustomerConnection.removeCookie();
+            CustomerConnection.Abort();
+            customer.Reset();
+            isConnected = false;
+        }
+
+        public void AgentEndsChat()
+        {
+            if (!isConnected)
+            {
+                return;
+            }
+            CustomerConnection.showChatEndMessage();
+            CustomerRemove();
+        }
+
+        public void CustomerEndsChat()
+        {
+            if (!isConnected)
+            {
+                return;
+            }
+            AgentConnection.showEndChatMessage();
+            CustomerRemove();
+        }
+
+        public void Send(string name, string message)
+        {
+            if (!isConnected) {
+                return;
+            }
+            CustomerConnection.broadcastMessage(name, message);
+            AgentConnection.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ Chat/ChatUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OfficeClip.LiveChat.Chat
+{
+    public class ChatUser
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ConnectionId { get; set; }
+        public bool IsAvailable
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(ConnectionId);
+            }
+        }
+
+        public void Reset()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            ConnectionId = string.Empty;
+        }
+    }
+}
