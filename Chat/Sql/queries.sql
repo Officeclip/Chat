1ffbf015ca611447052a34082d63218c0fc0f400
SK Dutta <skd@officeclip.com>
2018-09-22 21:49:55 -0400

    intermediate commit

--- Chat/Sql/queries.sql
+++ Chat/Sql/queries.sql
@@ -1,30 +1,58 @@
-﻿IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'GetUsersSP')
+﻿IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'InsSessionChatSP')
 BEGIN
-	PRINT 'Dropping Procedure GetUsersSP'
-	DROP  Procedure  GetUsersSP
+	PRINT 'Dropping Procedure InsSessionChatSP'
+	DROP  Procedure  InsSessionChatSP
 END
 
 GO
 
-PRINT 'Creating Procedure GetUsersSP'
+PRINT 'Creating Procedure InsSessionChatSP'
 GO
---
--- Gets the user-id given the email address
---
-Create Procedure GetUsersSP
+
+Create Procedure InsSessionChatSP
 (
+	@userName nvarchar(255),
 	@emailAddress nvarchar(255),
-	@isExternal bit,
-	@userId int output	-- returns -1 if no users found
+	@customValue nvarchar(255),
+	@connectionId varchar(255),
+	@chatSessionId int output
 )
 AS
-	SET @userId = (SELECT DISTINCT user_id
-	FROM users
-	WHERE  email_address = @emailAddress
-	AND is_external = @isExternal
-	AND active = 'Y')
+	DECLARE @endUserId int
+	SET @endUserId = (SELECT end_user_id from endusers where email = @emailAddress)
+	IF (@endUserId IS NULL)
+	BEGIN
+		INSERT INTO endusers
+		(name, email, connection_id)
+		VALUES
+		(@userName, @emailAddress, @connectionId)
+		SET @endUserId = (SELECT scope_identity())
+	END
+	INSERT INTO chatsessions
+	(end_user_id, custom_value, start_time, is_active)
+	VALUES
+	(@endUserId, @customValue, GETDATE(), 1)
+	SET @chatSessionId = (SELECT scope_identity())
+GO
+GRANT EXEC ON InsSessionChatSP TO PUBLIC
+GO
+--------------------------------------
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'GetUsersChatSP')
+BEGIN
+	PRINT 'Dropping Procedure GetUsersChatSP'
+	DROP  Procedure  GetUsersChatSP
+END
 
 GO
-GRANT EXEC ON GetUsersSP TO PUBLIC
+
+PRINT 'Creating Procedure GetUsersChatSP'
+GO
+
+Create Procedure GetUsersSP
+AS
+	SELECT * from users
+GO
+
+GRANT EXEC ON GetUsersChatSP TO PUBLIC
 GO
 ---------------------------
